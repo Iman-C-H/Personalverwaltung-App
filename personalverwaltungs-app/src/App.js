ed256cc53fd5ae8c920f6580d2c66a6f039655d3
Iman Ceylan Halim <halim_i@hotmail.de>
2024-11-13 22:28:28 +0100

    13.11.

--- personalverwaltungs-app/src/App.js
+++ personalverwaltungs-app/src/App.js
@@ -1,37 +1,75 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import React from "react";
 
+///// Hauptkomponente App /////
 function App() {
+  ///// Rendern der App /////
   return (
-    <div className="container">
-      <aside className="sidebar">
-        <ul>
-          <li><a href="1">Mitarbeiter</a></li>
-          <li><a href="2">Dokumente</a></li>
-          <li><a href="3">Urlaubs- und Abwesenheitsplan</a></li>
-          <li><a href="4">Offene Stellen</a></li>
-        </ul>
-      </aside>
+    <>
+      {/* Hauptcontainer */}
+      <div className="container">
+        {/* Sidebar */}
+        <aside className="sidebar">
+          <ul>
+            <li>
+              <a href="HR" className="sbbuttonID">
+                Techstarter HR
+              </a>
+            </li>{" "}
+            <br></br>
+            <br></br>
+            <li>
+              <a href="#1" className="sbbutton">
+                Mitarbeiter
+              </a>
+            </li>
+            <li>
+              <a href="#2" className="sbbutton">
+                Dokumente
+              </a>
+            </li>
+            <li>
+              <a href="#3" className="sbbutton">
+                Urlaubs/<br></br>Abwesenheitsplan
+              </a>
+            </li>
+            <li>
+              <a href="#4" className="sbbutton">
+                Offene Stellen
+              </a>
+            </li>
+          </ul>
 
-      <div className="main-content">
-       
-        <staffbase>
-          <img src="/logofirst.png" alt="StaffBase" className="logo"/>
-        </staffbase>
- <header className="animated">
-          <h1>Willkommen</h1>
+          <div className="main-content-flex">
+            <button className="button">+</button>{" "}
+            <h2>Mitarbeiter hinzufügen</h2>
+          </div>
+        </aside>
+
+        {/* Logo-Bereich */}        
+        <header className="animated-title">
+          <h1>Willkommen!</h1>
         </header>
-        <main>
-          <h3>Mit Staffbase lässt es sich managen.</h3>
-          <button className="button">Personal hinzufügen</button>
-        </main>
+        <div className="logo-container, animated-logo">
+          <img src="/Logosecond.png" alt="StaffBase" className="logo" />
+        </div>
+
+        <div>
+          <img src="/Bild.svg" alt="Illustration" />
+        </div>
+
+
+        {/* Hauptinhalt */}
+        {/* Hauptinhalt */}
       </div>
 
+      {/* Footer außerhalb des Hauptcontainers */}
       <footer className="footer">
         <p>&copy; 2024 StaffBase. Alle Rechte vorbehalten.</p>
       </footer>
-    </div>
+    </>
   );
 }
 
+///// Export der App-Komponente /////
 export default App;
