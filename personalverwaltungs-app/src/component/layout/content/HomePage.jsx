446a2edbae95e750dac5682ec939e479bdf1d92d
Iman Ceylan Halim <halim_i@hotmail.de>
2024-11-18 20:10:48 +0100

    Details.Iman

--- personalverwaltungs-app/src/component/layout/content/HomePage.jsx
+++ personalverwaltungs-app/src/component/layout/content/HomePage.jsx
@@ -1,18 +1,21 @@
 import { Router } from "react-router-dom";
 import SideBar from "../sidebar/Sidebar.jsx";
-import "./Homepage.module.css";
+import "./Homepage.css";
 
 function HomePage() {
   return (
 
-      <div>
+      <div>        
+        <div className="logo-container, animated-logo">
+          <img src="/Logosecond.png" alt="StaffBase" className="logo" />
+        </div>
+
+
         <header className="animated-title">
           <h1>Willkommen!</h1>
         </header>
 
-        <div className="logo-container, animated-logo">
-          <img src="/Logosecond.png" alt="StaffBase" className="logo" />
-        </div>
+
         
         <div>
             <SideBar/>
